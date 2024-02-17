e20a87bf14b24b70a908333d0713d418e99fcc2a
karthikns9 <karthikns999@gmail.com>
2024-02-17 12:13:08 +0530

    Add project files.

---
+++ OpenAiAssistant/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace OpenAiAssistant
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
